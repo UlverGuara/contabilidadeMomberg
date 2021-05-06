ceae0d41b6df6a871a82f03dc96bbd0e738b9ea7
Rafael Momberg de Andrade <rafahell.momberg@gmail.com>
2021-05-05 22:08:58 -0300

    Componentes Contato, Início, Serviços e Sobre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    InicioComponent,
+    ServicosComponent,
+    SobreComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
