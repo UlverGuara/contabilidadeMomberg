a128c2dd71f01c838a4c10b4c72975b60425adfb
Rafael Momberg de Andrade <rafahell.momberg@gmail.com>
2021-05-06 20:44:53 -0300

    Versão 1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { SobreComponent } from './sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'inicio', pathMatch: 'full'},
+
+  {path: 'inicio', component: InicioComponent},
+  {path: 'servicos', component: ServicosComponent},
+  {path: 'sobre', component: SobreComponent},
+  {path: 'contato', component: ContatoComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
